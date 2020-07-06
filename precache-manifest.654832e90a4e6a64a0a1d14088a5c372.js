dfc4b1e7fe505d032335ed789d2ac273f42ee0bb
naibahmed <naibahmed96@gmail.com>
2020-07-06 22:02:03 +0300

    Updates

---
+++ precache-manifest.654832e90a4e6a64a0a1d14088a5c372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a0241835fcdaf95d9d4e6bc0348b33",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b5319ddb57564dc474dc",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c9312a1846ca799e6e8",
+    "url": "./static/js/2.86b6448f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.86b6448f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5319ddb57564dc474dc",
+    "url": "./static/js/main.e4231a5d.chunk.js"
+  },
+  {
+    "revision": "d562289f2cf1b9ca1756",
+    "url": "./static/js/runtime-main.8a1fd9d9.js"
+  }
+]);
