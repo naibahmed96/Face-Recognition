b9b07506b817a37f4a9b9b96b134a998faab8535
naibahmed <naibahmed96@gmail.com>
2020-07-10 09:27:58 +0300

    Added Particle.js to background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import Particles from "react-particles-js";
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const particlesOption = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        area: 800,
+        factor: 1000
+      }
+    },
+    
+  }
+};
+
 function App() {
   return (
     <div className="App">
+      <Particles
+        params={particlesOption}
+        className="particles"
+      />
       <Navigation />
       <Logo />
       <Rank />
