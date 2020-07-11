1fe0587178a6761fbe5600d514dec16ef19b2e08
naibahmed <naibahmed96@gmail.com>
2020-07-11 22:15:39 +0300

    Added Basic eventListeners on ImageLinkForm input and button component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Particles from "react-particles-js";
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -20,20 +20,39 @@ const particlesOption = {
   }
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles
-        params={particlesOption}
-        className="particles"
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonClick = () => {
+    console.log('Clicked');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles params={particlesOption} className="particles" />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange} 
+          onButtonClick={this.onButtonClick}
+        />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
